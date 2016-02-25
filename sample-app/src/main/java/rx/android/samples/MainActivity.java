edd4b92e8a7fbdf0e759298869c8191482198064
林尧 <1542319159@qq.com>
2016-02-25 14:36:02 +0800

    添加注释

--- sample-app/src/main/java/rx/android/samples/MainActivity.java
+++ sample-app/src/main/java/rx/android/samples/MainActivity.java
@@ -1,11 +1,19 @@
 package rx.android.samples;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import java.util.concurrent.TimeUnit;
 import rx.Observable;
 import rx.Subscriber;
@@ -21,22 +29,38 @@ public class MainActivity extends Activity {
 
     private Handler backgroundHandler;
 
-    @Override protected void onCreate(Bundle savedInstanceState) {
+    private Button button;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.main_activity);
 
+        //创建一个后台线程
         BackgroundThread backgroundThread = new BackgroundThread();
         backgroundThread.start();
+        //创建一个处理者
         backgroundHandler = new Handler(backgroundThread.getLooper());
 
+        //给按钮绑定点击事件
         findViewById(R.id.button_run_scheduler).setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View v) {
+            @Override
+            public void onClick(View v) {
                 onRunSchedulerExampleButtonClicked();
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
+    //点击之后所做的事情
     void onRunSchedulerExampleButtonClicked() {
         sampleObservable()
                 // Run on a background thread
@@ -44,32 +68,85 @@ public class MainActivity extends Activity {
                 // Be notified on the main thread
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Subscriber<String>() {
-                    @Override public void onCompleted() {
-                        Log.d(TAG, "onCompleted()");
+                    //处理完成发生异常
+                    @Override
+                    public void onCompleted() {
+                        Log.d(TAG, "处理完了");
                     }
 
-                    @Override public void onError(Throwable e) {
+                    @Override
+                    public void onError(Throwable e) {
                         Log.e(TAG, "onError()", e);
                     }
 
-                    @Override public void onNext(String string) {
+                    // 打印的string 是 "one", "two", "three", "four", "five"
+                    @Override
+                    public void onNext(String string) {
+                        if (string=="one"){
+                            System.out.println("我是1");
+                        }
                         Log.d(TAG, "onNext(" + string + ")");
                     }
                 });
     }
 
-    static Observable<String> sampleObservable() {
+    //返回一个观察者
+    Observable<String> sampleObservable() {
         return Observable.defer(new Func0<Observable<String>>() {
-            @Override public Observable<String> call() {
+            @Override
+            public Observable<String> call() {
                 try {
                     // Do some long running operation
-                    Thread.sleep(TimeUnit.SECONDS.toMillis(5));
+                    Thread.sleep(TimeUnit.SECONDS.toMillis(2));
+                    Toast.makeText(getApplicationContext(), "默认Toast样式",
+                            Toast.LENGTH_SHORT).show();
                 } catch (InterruptedException e) {
                     throw OnErrorThrowable.from(e);
                 }
+                //发送标志
                 return Observable.just("one", "two", "three", "four", "five");
             }
         });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://rx.android.samples/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://rx.android.samples/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 
     static class BackgroundThread extends HandlerThread {
